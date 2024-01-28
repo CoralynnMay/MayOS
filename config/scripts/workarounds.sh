c5b8e0ef16150f81cc994d3ecc829a15c0df9ed1
CoralynnMay <su@coralynn.net>
2024-01-28 14:31:56 -0500

    Update workarounds.sh

--- config/scripts/workarounds.sh
+++ config/scripts/workarounds.sh
@@ -4,4 +4,4 @@ set -oeux pipefail
 
 # alternatives cannot create symlinks on its own during a container build
 ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
-
+/usr/sbin/alternatives --install /usr/bin/emacs emacs /usr/bin/emacs-29.2 80
