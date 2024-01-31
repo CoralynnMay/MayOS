df62239d6304cb2f604af1d1cd4d075992e80eb0
CoralynnMay <CoralynnMay@users.noreply.github.com>
2024-01-31 15:29:45 -0500

    fix: Emacs too transparent by default

--- config/files/usr/etc/skel/.config/doom/config.el
+++ config/files/usr/etc/skel/.config/doom/config.el
@@ -74,4 +74,4 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(add-to-list 'default-frame-alist '(alpha-background . 90))
+(add-to-list 'default-frame-alist '(alpha-background . 95))
