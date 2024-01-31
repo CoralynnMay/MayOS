5335a0da2d82fb6c4fc90a00b5362544d843f64b
CoralynnMay <CoralynnMay@users.noreply.github.com>
2024-01-30 20:45:08 -0500

    fix: switch to bitwarden rpm from official source

---
+++ config/scripts/install-bitwarden.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+curl -o /tmp/bitwarden.rpm -L "https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=rpm"
+rpm-ostree install /tmp/bitwarden.rpm
